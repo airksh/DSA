81fc6f26cf34cf369629061e2e42480302e2524e
Rishu Kumar <71171643+airksh@users.noreply.github.com>
2022-06-02 00:28:05 +0530

    Update UnionFind.py

--- UnionFind.py
+++ UnionFind.py
@@ -19,8 +19,8 @@ class UnionFind():
         self.root[x] = self.find(self.root[x])
         return self.root[x]
     def union(self, x, y):
-        root_x = self.root[x]
-        root_y = self.root[y]
+        root_x = self.find(x)
+        root_y = self.find(y)
         if root_x != root_y:
             if self.rank[root_x] > self.rank[root_y]:
                 self.root[root_y] = root_x
