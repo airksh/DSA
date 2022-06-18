4e8d0747a4c850c66f2315dd3bb0941f7722b166
Rishu Kumar <71171643+airksh@users.noreply.github.com>
2022-06-18 22:15:33 +0530

    Update Trie (Prefix Tree).py

--- Trie (Prefix Tree).py
+++ Trie (Prefix Tree).py
@@ -1,4 +1,5 @@
-# A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. 
+# There are various applications of this data structure, such as autocomplete and spellchecker.
 
 # Implement the Trie class:
 
