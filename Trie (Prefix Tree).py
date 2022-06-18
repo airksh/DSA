fc643a0db8f5f05cfc8ac21cb8f6ba589b4712d1
Rishu Kumar <71171643+airksh@users.noreply.github.com>
2022-06-18 22:14:41 +0530

    Create Trie (Prefix Tree).py

---
+++ Trie (Prefix Tree).py
@@ -0,0 +1,53 @@
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+# Implement the Trie class:
+
+# --> Trie() Initializes the trie object.
+# --> void insert(String word) Inserts the string word into the trie.
+# --> boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+# --> boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return curr.end
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for ch in prefix:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return True
+    
+    # TC Insert, Search and Starts with: O(26*w)
+    # SC Insert, Search and Starts with: O(26*w)
+
+
+# Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
